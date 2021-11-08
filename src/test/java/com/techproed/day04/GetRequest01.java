fb876d517e76231a9752c0127f98b3490036fc33
ferudungogus <86746223+ferudungogus@users.noreply.github.com>
2021-11-09 00:21:13 +0300

    day4 gün sonu

--- src/test/java/com/techproed/day04/GetRequest01.java
+++ src/test/java/com/techproed/day04/GetRequest01.java
@@ -1,5 +1,7 @@
 package com.techproed.day04;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 public class GetRequest01 {
@@ -18,10 +20,29 @@ public class GetRequest01 {
         //2- beklenen sonuç(expected result) oluşturulur.
         // bu case de benden body doğrulaması istenmediği için şimdilik beklenen sonuç oluşturmuyoruz
         //3-  request gönder
-        Response response= given().accept("application/json").when().get(url);//request oldu ve bunu response atadık
-        //4-actual result oluştur
-        //5-doğrulama yap(assertion)
+        Response response= given().
+                accept("application/json").
+                when().
+                get(url);//request oldu ve bunu response atadık
         response.prettyPrint();
+        //4-actual result oluştur
+        //response bıdy ile ilgili işlem yapılmayacağı için simdi olusturmayacağız
+        //5-doğrulama yap(assertion)
+        System.out.println(response.getStatusCode());//200
+        System.out.println(response.getContentType());//application/json; charset=utf-8
+        System.out.println(response.getStatusLine());//HTTP/1.1 200 OK
+        System.out.println(response.getHeaders());//bütün headerı alır ve yazdırır
+
+        Assert.assertEquals(200,response.getStatusCode());
+        //status code int deger döner
+        Assert.assertEquals("expected ile actual degerler eşit değil","HTTP/1.1 200 OK",response.getStatusLine());
+
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).//application/json de yazılabilir
+                statusLine("HTTP/1.1 200 OK");
+
     }
 }
 
