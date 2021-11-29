d8991c321970314791bdcf9ee64eeba7f083569b
ferudungogus <86746223+ferudungogus@users.noreply.github.com>
2021-11-29 22:03:59 +0300

    2911

--- src/test/java/com/techproed/day12/PostRequestWithPojo02.java
+++ src/test/java/com/techproed/day12/PostRequestWithPojo02.java
@@ -1,7 +1,15 @@
 package com.techproed.day12;
 
+import com.techproed.pojos.BookingDatePojo;
+import com.techproed.pojos.BookingPojo;
+import com.techproed.pojos.BookingResponsePojo;
 import com.techproed.testBase.HerokuAppTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class PostRequestWithPojo02 extends HerokuAppTestBase {
     //https://restful-booker.herokuapp.com/booking
@@ -33,6 +41,23 @@ public class PostRequestWithPojo02 extends HerokuAppTestBase {
 @Test
     public void test(){
 
+spec02.pathParam("parametre1","booking");
+    BookingDatePojo bookingdates =new BookingDatePojo("2020-09-09","2020-09-21");
+    System.out.println(bookingdates);
+    BookingPojo bookingPojo=new BookingPojo("Selim","Ak",15000,true,bookingdates);
+    System.out.println(bookingPojo);
+    Response response=given().contentType(ContentType.JSON).spec(spec02).auth().basic("admin","password123").
+            body(bookingPojo).when().post("/{parametre1}");
+response.prettyPrint();
+
+    BookingResponsePojo actualData=response.as(BookingResponsePojo.class);
+    Assert.assertEquals(200,response.getStatusCode());
+    Assert.assertEquals(bookingPojo.getFirstname(),actualData.getBooking().getFirstname());
+    Assert.assertEquals(bookingPojo.getLastname(),actualData.getBooking().getLastname());
+    Assert.assertEquals(bookingPojo.getTotalprice(),actualData.getBooking().getTotalprice());
+    Assert.assertEquals(bookingPojo.isDepositpaid(),actualData.getBooking().isDepositpaid());
+    Assert.assertEquals(bookingPojo.getBookingdates().getCheckin(),actualData.getBooking().getBookingdates().getCheckin());
+    Assert.assertEquals(bookingPojo.getBookingdates().getCheckout(),actualData.getBooking().getBookingdates().getCheckout());
 
 
 }
